faeb9270854548f83d110bcf1bef197c2e95e918
Stanley Opare <stanleyopare@Stanleys-MacBook-Air.local>
2023-08-07 11:01:35 -0600

    All scripts, images and README.md files complete

---
+++ README.md
@@ -0,0 +1,38 @@
+
+  # SVG LogoMaker
+  
+  ## Description
+  One of the most common features one will find on a webpage is a logo. It communicates what the webpage is about and what services they render. It is often an abbreviation of the company's name. One of the ways to make life easy for oneself is to write a code that can help web developers easily generate a logo by just answering and choosing shapes and colors and other features of interest. This project seeks to answer that by using preset colours and shapes. The shapes used are square, circle and triangle.
+  
+  ## Table of Contents
+  - [Installations](#installations)
+  - [Usage](#usage)
+  - [Appearance and  Functionality](#Appearance&Functionality)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [Credits](#Credits)
+  
+  ## Installations
+  1. Run npm init -y in a terminal. 
+  2. Install "[Inquirer](https://www.npmjs.com/package/inquirer/v/8.2.4)" and "[Jest](https://jestjs.io/docs/getting-started)" packages by typing and entering in a console "npm install inquirer" and "npm install -g jest". 
+  
+  ## Usage
+  To run the logo generator type and enter in a terminal "node index.js" and answer all onscreen questions.
+
+  ## Appearance and  Functionality
+  Click on [SVG_Logomaker](https://watch.screencastify.com/v/yhLES0HbQH4XGFXqf9IP) to watch the video walkthrough.
+  ![Onscreen Logo generator](8A125A80-5E28-4CEF-BBD7-E5A0DF0C15AF.jpeg)
+
+  ## Tests
+  Type and enter either "npm test" or "jest" in a terminal. 
+  
+  ## Questions
+  For any questions or suggestions, feel free to reach out to me on GitHub: [StanOp09](https://github.com/StanOp09) or via email: stanleykaopare@gmail.com.
+  
+  ## Credits
+  - Sean Roshan
+  - Chat GPT
+  - [NPM](https://www.npmjs.com/)
+  - [NodeJS](https://nodejs.org/en)
+  - [Inquirer](https://www.npmjs.com/package/inquirer)
+  - [Jest](https://jestjs.io/docs/getting-started)
--- index.js
+++ index.js
@@ -1,67 +1,44 @@
-const fs = require('fs');
 const inquirer = require('inquirer');
-const SVG = require('svg.js');
+const { generateSVG } = require('./lib/logoGenerator');
+const { isValidShape } = require('./lib/shapes');
+const { isValidColor } = require('./lib/colors');
 
-// Function to create the logo SVG file
-async function createLogo(text, textColor, shape, shapeColor) {
-  const width = 300;
-  const height = 200;
+const runLogoGenerator = async () => {
+  try {
+    const userInput = await inquirer.prompt([
+      {
+        name: 'text',
+        message: 'Enter up to three characters:',
+        validate: (input) => input.length > 0 && input.length <= 3,
+      },
+      {
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hexadecimal number):',
+        validate: (input) => isValidColor(input),
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square'],
+      },
+      {
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hexadecimal number):',
+        validate: (input) => isValidColor(input),
+      },
+    ]);
 
-  // Create SVG canvas
-  const canvas = SVG(document.documentElement).size(width, height);
+    if (!isValidShape(userInput.shape)) {
+      throw new Error('Invalid shape choice. Please choose from circle, triangle, or square.');
+    }
 
-  // Draw shape
-  switch (shape) {
-    case 'circle':
-      canvas.circle(100).move(100, 50).fill(shapeColor);
-      break;
-    case 'triangle':
-      canvas.polygon('50,150 150,150 100,50').fill(shapeColor);
-      break;
-    case 'square':
-      canvas.rect(100, 100).move(100, 50).fill(shapeColor);
-      break;
-    default:
-      console.log('Invalid shape.');
-      return;
+    generateSVG(userInput.text, userInput.textColor, userInput.shape, userInput.shapeColor);
+
+    console.log('Generated logo.svg');
+  } catch (error) {
+    console.error('Error:', error.message);
   }
+};
 
-  // Draw text
-  canvas.text(text).move(130, 120).font({ size: 40, fill: textColor });
-
-  // Save SVG to file
-  fs.writeFileSync('logo.svg', canvas.svg());
-  console.log('Generated logo.svg');
-}
-
-// Prompt user for input
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter up to three characters:',
-    },
-    {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter text color (keyword or hexadecimal number):',
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Choose a shape:',
-      choices: ['circle', 'triangle', 'square'],
-    },
-    {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter shape color (keyword or hexadecimal number):',
-    },
-  ])
-  .then((answers) => {
-    createLogo(answers.text, answers.textColor, answers.shape, answers.shapeColor);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+runLogoGenerator();
